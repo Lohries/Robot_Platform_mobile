b47d8e69cafdff6d09584628650f5c4dc4063a4a
Lohries <lohries@Lohriess-MacBook-Air.local>
2024-06-27 11:54:13 -0300

    initial page done

--- App.js
+++ App.js
@@ -4,17 +4,35 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> Inmoov </Text>
       <StatusBar style="auto" />
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>O Aplicativo visa fazer a comunicação com o hardware fisico, além de ter determindas funcoes que extrapolam as ideias iniciais do projeto</Text>
+      </View>
     </View>
+   
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3dfdcd',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingBottom: '20%', 
+  },
+  footer: {
+    position: 'absolute', 
+    bottom: 0,
+    backgroundColor: 'gray',
+    width: '100%',
+    height: '20%',
+    justifyContent: 'center',
+    alignItems: 'center', 
+  },
+  footerText: {
+    color: 'white',
   },
 });
